79f06c3ceea92f6ef029f3b6160e14f7be399e3d
M1043307 <M1043307@A2ML17727.mindtree.com>
2018-03-20 13:44:34 +0530

    folder restrucure

--- test/test.java
+++
@@ -1,3 +0,0 @@
-ttest:wq
-
-test
---
+++ test/test/TestBankAccount.java
@@ -0,0 +1,26 @@
+package test;
+
+import junit.framework.Assert;
+import pack.BankAccount;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class TestBankAccount {
+
+	@Test
+	public void testDebitWithSufficientFunds() {
+		BankAccount account = new BankAccount(10);
+		double amount = account.debit(5.0);
+		Assert.assertEquals(5.0, amount);
+
+	}
+
+	@Test
+	public void testDebitWithInSufficientFunds() {
+		BankAccount account = new BankAccount(11);
+		double amount = account.debit(5.0);
+		Assert.assertEquals(10.0, amount);
+
+	}
+}
