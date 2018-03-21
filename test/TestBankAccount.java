c9acc0765a989e0832d6e57418397feec3cf8937
M1043307 <M1043307@A2ML17727.mindtree.com>
2018-03-21 11:55:48 +0530

    test

---
+++ test/TestBankAccount.java
@@ -0,0 +1,28 @@
+
+
+import junit.framework.Assert;
+import src.main.java.BankAccount;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class TestBankAccount {
+
+	@Test
+	public void testDebitWithSufficientFunds() {
+		BankAccount account = new BankAccount(10);
+		double amount = account.debit(5.0);
+		Assert.assertEquals(5.0, amount);
+
+	}
+	
+	//test
+
+	@Test
+	public void testDebitWithInSufficientFunds() {
+		BankAccount account = new BankAccount(11);
+		double amount = account.debit(5.0);
+		Assert.assertEquals(10.0, amount);
+
+	}
+}
